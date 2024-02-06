cd5b2ef5673842fa49021bb58d89a9352a3dad6b
Shayan Abbas <shayanabbas@outlook.com>
2024-02-06 15:40:48 +0200

    added signup functionality and authentication

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,46 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/core/core.dart';
+import 'package:twitter_clone/core/providers.dart';
+
+final authAPIProvider = Provider((ref) {
+  final account = ref.watch(appwriteAccountProvider);
+  return AuthAPI(account: account);
+});
+
+abstract class IAuthAPI {
+  FutureEither<model.User> signUp({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthAPI implements IAuthAPI {
+  final Account _account;
+  AuthAPI({required Account account}) : _account = account;
+
+  @override
+  FutureEither<model.User> signUp({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final account = await _account.create(
+        userId: ID.unique(),
+        email: email,
+        password: password,
+      );
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? 'Some unexpected error occoured!', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(
+        Failure(e.toString(), stackTrace),
+      );
+    }
+  }
+}
